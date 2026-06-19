e4a8824640892a84cd2aae68fd6c68cd6a30f3bd
Diego <dglvr3@gmail.com>
2026-06-19 16:26:21 -0300

    refactor: update Arrays2 exercise

--- src/main/java/dsa/arrays/Arrays2.java
+++ src/main/java/dsa/arrays/Arrays2.java
@@ -1,8 +1,17 @@
 package dsa.arrays;
 
+import java.util.Arrays;
+
 public class Arrays2 {
     // Leetcode 1929. Concatenation of Array
-    public int[] getConcatenation(int[] nums) {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 1};
+        System.out.println("Expected output: [1, 2, 1, 1, 2, 1]");
+        System.out.println("Actual output:   " + Arrays.toString(getConcatenation(nums)));
+    }
+
+    public static int[] getConcatenation(int[] nums) {
+        // O(n) complexity
         int[] ans = new int[nums.length * 2];
         for (int i = 0; i < ans.length / 2; i++) {
             ans[i] = nums[i];
