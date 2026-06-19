cf41242c5966f7964102dd5984b567cb01ee96bf
Diego <dglvr3@gmail.com>
2026-06-19 16:25:53 -0300

    refactor: update Arrays1 exercise to avoid index out of bounds exception

--- src/main/java/dsa/arrays/Arrays1.java
+++ src/main/java/dsa/arrays/Arrays1.java
@@ -1,10 +1,19 @@
 package dsa.arrays;
 
+import java.util.Arrays;
+
 public class Arrays1 {
     // Leetcode 1480. Running Sum of 1d Array
-    public int[] runningSum(int[] nums) {
-        for (int i = 1; i <= nums.length; i++) {
-            nums[i] += nums[i-1];
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4};
+        System.out.println("Expected output: [1, 3, 6, 10]");
+        System.out.println("Actual output:   " + Arrays.toString(runningSum(nums)));
+    }
+
+    public static int[] runningSum(int[] nums) {
+        // O(n) complexity
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
         }
         return nums;
     }
